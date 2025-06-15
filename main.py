4e3d30c73f2413712e23dec38356d64814b53cfd
Michael <mushymike1996@gmail.com>
2025-06-16 01:22:48 +1000

    added player ship with triangle rendering

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
+from player import Player
+
+
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    black = (0,0,0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
-        screen.fill(black)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
